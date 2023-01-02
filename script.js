a102c555197ec0374792c1b4f79cf1c2a0d0e7aa
Manish Pokhriyal <Manish.pokhriyal.666@gmail.com>
2023-01-02 15:57:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'f1fee8667emsh65f3ceba20c4a22p11b677jsne5526e3d36e9',
+		'X-RapidAPI-Key': '**Secret**',
 		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
 	}
 };
